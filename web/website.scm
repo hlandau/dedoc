3712f9938551d61b83df7ec7297d07362b4bc223
Hugo Landau <hlandau@devever.net>
2023-01-28 12:04:23 +0000

    Website
    ©! I, Hugo Landau <hlandau@devever.net>, hereby licence these changes under the
    ©! licence with SHA256 hash
    ©! fd80a26fbb3f644af1fa994134446702932968519797227e07a1368dea80f0bc.

--- web/website.scm
+++ web/website.scm
@@ -9,10 +9,14 @@ set -e; exec guile --fresh-auto-compile --no-auto-compile -L "$(dirname "$0")" -
 (define-public ns-xhtml "http://www.w3.org/1999/xhtml")
 
 (define _DEDOC "DEDOC")
-(define _dedoc `(tt "dedoc.scm"))
+(define _dedoc "dedoc")
+(define _dedoc.scm `(tt "dedoc.scm"))
+(define _dedoc-methods `(tt "dedoc-methods"))
+(define _acmetool `(a (@ (href "https://www.devever.net/~hl/acmetool/")) "acmetool"))
+(define _context "ConTeXt")
 
 (define* (page-layout #:key
-                      (title "DEDOC")
+                      (title _dedoc)
                       (body '()))
   `(*TOP*
      (html (@ (@ (*NAMESPACES* (*IMPLICIT* ,ns-xhtml    *IMPLICIT*)))
@@ -29,7 +33,7 @@ set -e; exec guile --fresh-auto-compile --no-auto-compile -L "$(dirname "$0")" -
                  (ul (@ (class rhs))
                    (li (a (@ (href "https://www.devever.net/~hl/") (class cross-slash-icon-link)) (span) "Hugo Landau")))
                  (ul (@ (class lhs))
-                   (li (a (@ (href ".") (class logotext)) "DEDOC"))
+                   (li (a (@ (href ".") (class logotext)) "dedoc"))
                    (li (a (@ (href "schema/")) "Schema Definition"))
                    (li (a (@ (href "examples")) "Examples"))
                    (li (a (@ (href "tutorial")) "Getting Started"))
@@ -38,7 +42,7 @@ set -e; exec guile --fresh-auto-compile --no-auto-compile -L "$(dirname "$0")" -
                     (p "⇒ Swipe right for navigation ⇒"))
                (div (@ (class prealign))
                     (h1 (@ (class logo-img))
-                        "DEDOC"))
+                        "dedoc"))
                (main
                  (article (@ (class has-logo))
                     ,@body))
@@ -60,59 +64,103 @@ set -e; exec guile --fresh-auto-compile --no-auto-compile -L "$(dirname "$0")" -
 
 
 (define (intro)
-  `((p "DEDOC is three things:")
+  `(
+    (p ,_dedoc" is a system for writing natural-language technical
+       documentation or technical standards which focuses on providing a
+       powerful writing environment while refusing to compromise on quality of
+       output.")
+
+    (p ,_dedoc" as a whole comprises three fundamental parts:")
     (ul
-      (li (p ,_DEDOC", an XML schema for writing high-quality technical documentation
+      (li (p ,_DEDOC", an "(a (@ (href "schema/")) "XML schema")" for writing high-quality technical documentation
               while targeting multiple different output formats, including
-              XHTML, PDF, EPUB and plain text;"))
-      (li (p ,_dedoc", an environment for writing technical documentation in Scheme
-              targeting the "DEDOC" schema."))
-      (li (p "a set of makefile-driven methods for converting ",_DEDOC" files to various output formats,
+              XHTML, PDF, EPUB, manpages and plain text, while also being simpler and
+              more focused than DocBook;"))
+      (li (p ,_dedoc.scm", an environment for writing technical documentation in Scheme
+              targeting the "DEDOC" schema, providing a friendlier writing
+              environment for hypertext than writing XML directly and allowing
+              procedural generation of content using arbitrary Scheme code;"))
+      (li (p ,_dedoc-methods", a set of Makefile-driven methods for converting ",_DEDOC" files to various output formats,
               such as high-quality PDF, XHTML, EPUB, plain text and man pages.")))
-    (p "In short, ",_dedoc" is a system for writing natural-language technical
-        documents using Scheme. By using the power of S-expressions, including
-        ordinary functions, macros and backquoting, and SXML (the
-        representation of XML in S-expressions), you can ergonomically write
-        documents which compile to highly semantic, clean XML in whatever schema
-        you desire.")
 
-    (p "You can then use the power of SXML (or, if you wish, any external XML
-        manipulation tooling of your choice, such as an XSLT processor) to “lower”
-        this highly semantic and platonic XML representation of your document into
-        a more concrete representation, such as DocBook, XHTML or XSL-FO;
-        or you can use a Scheme transformer which consumes your SXML and
-        produces a non-XML representation such as LaTeX or ConTeXt.")
+    (p (strong "Scheme writing environment. ") "In short, ",_dedoc" is a system
+       for writing natural-language technical documents using Scheme. By using
+       the power of S-expressions, including ordinary functions, macros and
+       backquoting, and SXML (the representation of XML in S-expressions), you
+       can ergonomically write documents which compile to a highly semantic,
+       clean XML representation:")
 
-    (p "The purpose of ",_DEDOC" is to allow the production of technical documents
-        which can be consumed as XHTML and come across as documents which were
-        designed for optimal consumption as XHTML; "(em "and")" which can be
-        consumed as PDFs and come across as documents which were designed for optimal
-        consumption as PDFs. In other words, ",_DEDOC" seeks to avoid any compromise
-        as to the quality of the output.")
+    (pre "\
+(define (introduction)
+  (sec \"Introduction\"
+       (p \"This is an example of writing a DEDOC section with a single
+            paragraph. We can also procedurally generate content:\")
 
-    (p "Because ",_DEDOC" documents are literally written in a full Scheme environment,
-        the system is also optimally suited to generating documents with large amounts
-        of generated content; for example, register manuals. You can use Scheme to
-        import arbitrary machine-readable data from any source and trivially transform
-        it into SXML. Moreover, since DEDOC can be used to produce high-quality,
-        semantic XML documents, those resulting documents are easily machine-read to
-        extract the embedded information. Why have people read through a 10,000 page
-        PDF of registers, when you can provide them with an XML file which
-        is both viewable in a web browser and machine intelligible, allowing
-        automated processing of all register definitions? You can either use XHTML
-        directly with embedded domain-specific semantic annotations, or a domain-specific
-        XML representation which is rendered viewable in a web browser via an attached
-        XSLT or CSS stylesheet.")
+       (map (lambda (name)
+              (p \"This is \"name\"'s paragraph.\"))
+            (list \"Alice\" \"Bob\" \"Charlie\"))
 
-     (p "Though ",_DEDOC" is the reference schema for ",_dedoc", it can also be used
-         with any other schema, such as a schema of your own definition.")
+       (p \"p is just a function which simply provides a convenience for
+            writing SXML. We can also write SXML directly using
+            quasiquotation:\")
 
-     (p ,_dedoc" is tiny. The core of it is a very small amount of ergonomic glue
-        built on top of SXML. You can consider it a demonstration of the power
-        of Scheme.")
+       `(p (@ (class \"the-answer\"))
+           \"The answer to life, the universe, and everything is \"
+           ,(* 6 7) \".\")
 
-     (p "Because documents are written in Scheme, you can ergonomically reference other objects
-         inline in text via lexical references. For example:")
+        )))")
+
+    (p (strong "Internal and external transformation. ")"You can then use the
+       power of SXML (or, if you wish, any external XML manipulation tooling of
+                          your choice, such as an XSLT processor) to “lower”
+       this semantic, platonic XML representation of your document into a more
+       concrete representation, such as DocBook, XHTML or XSL-FO; or you can
+       use a Scheme transformer which consumes your SXML and produces a non-XML
+       representation such as LaTeX, ConTeXt or roff. You can also use Scheme
+       or external processing tools to transform the (S)XML document prior to
+       lowering, for example by automatically adding section numbering, tables
+       of contents or similar, or by compiling figure images from their source
+       representation.")
+
+    (p (strong "High quality output. ")"The purpose of ",_dedoc" is to allow
+       the production of technical documents which can be consumed as XHTML and
+       come across as documents which were designed for optimal consumption as
+       XHTML; "(em "and")" which can be consumed as PDFs and come across as
+       documents which were designed for optimal consumption as PDFs. In other
+       words, ",_dedoc" seeks to avoid any compromise as to the quality of the
+       output.")
+
+    (p (strong "Machine generation and readability. ") "Because documents are
+       literally written in a full Scheme environment, the system is also
+       optimally suited to generating documents with large amounts of generated
+       content; for example, register manuals. You can use Scheme to import
+       arbitrary machine-readable data from any source and trivially transform
+       it into SXML. Moreover, since ",_dedoc" can be used to produce
+       high-quality, semantic XML documents, those resulting documents are
+       easily machine-read to extract the embedded information. Why have people
+       read through a 10,000 page PDF of registers, when you can provide them
+       with an XML file which is both viewable in a web browser and machine
+       intelligible, allowing automated processing of all register definitions?
+       You can either use XHTML directly with embedded domain-specific semantic
+       annotations, or a domain-specific XML representation which is rendered
+       viewable in a web browser via an attached XSLT or CSS stylesheet.")
+
+     (p (strong "Generalisable to other schemas. ") "Though ",_DEDOC" is the
+        reference schema for ",_dedoc", the principles used are fully generalisable
+        and ",_dedoc.scm" amounts to little more than a handful of utility functions
+        and macros to make writing SXML slightly more ergonomic. Thus, the entire
+        system could be readily adapted to use with any other schema.")
+
+     (p (strong "Small size. ") ,_dedoc" is tiny. The core of it is a very
+        small amount of ergonomic glue which simply makes writing SXML for the
+        target schema (",_DEDOC") slightly easier. You can consider it a
+        demonstration of the power of Scheme.")
+
+     (p (strong "Checked lexical references. ") "Because documents are written
+        in Scheme, you can ergonomically reference other objects inline in text
+        via lexical references. For example, when using ",_DEDOC", terminology
+        (as might be found in a definitions section of a specification) is
+        defined like so:")
 
      (pre ";; Define a term. This can be referenced lexically. All terms are collated
 ;; in a list inside the Scheme environment, and can be mapped over
@@ -136,25 +184,78 @@ set -e; exec guile --fresh-auto-compile --no-auto-compile -L "$(dirname "$0")" -
 
       (p "Note that this is not a special parsing environment. "(tt "dt")", "(tt "dpword")" and "(tt "p")"
           are simply Scheme macros or functions which define items in the
-          Scheme lexical environment. The full power of Scheme can be used to
-          produce generated output. A ",_dedoc" document is simply a Scheme
-          program which, when executed, outputs XML.")
+          Scheme lexical environment.")
+
+      (p "In the example above, "(tt "table")" and "(tt "MUST")" are simply
+         lexical references in the Scheme environment to objects which have
+         been defined in that environment via the previous "(tt "dt")" and "(tt
+         "dpword")" macro invocations.")
+      (p "Note that unlike some other languages, Scheme
+         does not require you to place spaces between quoted strings and other
+         tokens. This allows you to form the habit of simply putting all terminology
+         in quotes to reference it (a style which is reminiscent of that used by
+         some contract lawyers when referencing terminology). Although in actuality
+         what you're doing here is ending a string constant, referencing an object
+         and then starting a new string constant, it's easy to localise your
+         vision to make your eyes “forget” this and act as though you're
+         actually opening and closing a semantic construct, rather than closing
+         and opening one.")
+
+      (p (strong "Scheme runtimes. ")"The full power of Scheme can be used to
+         produce generated output. A ",_dedoc" document is simply a Scheme
+         program which, when executed, outputs XML.")
 
       (p ,_dedoc" is intended to be used with Guile Scheme, though it's possible
          it could be ported to other Schemes.")
+
+      (p (strong ,_dedoc-methods ". ") ,_dedoc" comes with a collection of
+         typesetting methods known as ",_dedoc-methods". Methods are split into different
+         tiers depending on the priority they are given in terms of support and maintenance.
+         The highest tier is Tier 1, which includes:")
+
+      (ul
+        (li (tt "pdf-context")", which uses ",_context"'s XML input support
+            to generate high-quality PDF output directly from ",_DEDOC" XML.
+            A standard ",_context" XML template is included, though you can
+            always provide your own or customise the included one. Requires ",_context".")
+        (li (tt "xhtml-single-xsl1")", which produces a single XHTML file
+            from ",_DEDOC" XML using an XSLT1 transform. Uses "(tt "xsltproc")".")
+        (li (tt "epub-xsl1")", which produces an EPUB file from ",_DEDOC" XML
+            using the "(tt "xhtml-single-xsl1")" method and a subsequent
+            transform. Requires "(tt "zip")".")
+        (li (tt "mdoc-xsl1")", which produces a "(tt "mdoc")"-format roff-style file
+            from ",_DEDOC" XML using an XSLT1-based transform. Requires "(tt
+            "xsltproc")" and XMLStarlet."))
+
+      (p "Tier 2 currently includes:")
+
+      (ul
+        (li (tt "txt-mandoc-mdoc-xsl1")", an example of generating plain text
+            (tier 1), PostScript, PDF or XHTML (tier 2"(sup †)") output using the "(tt
+            "mandoc(1)")" program.")
+        (li (tt "txt-roff-mdoc-xsl1")", an example of generating plain
+            text (tier 1), PostScript, PDF or XHTML (tier 2"(sup †)") output using the
+            "(tt "groff(1)")" program."))
+
+      (p (sup †)"The "(tt "mandoc(1)")" and "(tt "groff(1)")" workflows are primarily intended
+          to be used for their plain text output. Since they are also capable of producing
+          PostScript, PDF and (X)HTML output, this functionality is also exposed for
+          demonstration purposes, however this output will not be as high quality as
+          the dedicated PDF and XHTML methods above.")
+
+      (p (strong "Current usage. "),_dedoc" is used to produce the documentation for ",_acmetool".")
         ))
 
 (define-public (web-index)
-  (page-layout #:title "DEDOC Document Engineering Language" #:body `(
-    (h1 "DEDOC Document Engineering Language")
-    ,(intro))))
+  (let ((title (string-append "The "_dedoc" Technical Documentation System")))
+    (page-layout #:title title #:body `((h1 ,title) ,(intro)))))
 
 (define (tutorial)
   `((p "...")))
 
 (define-public (web-tutorial)
-  (page-layout #:title "DEDOC Tutorial" #:body `(
-    (h1 "DEDOC Tutorial")
+  (page-layout #:title (string-append _dedoc" Tutorial") #:body `(
+    (h1 ,_dedoc" Tutorial")
     ,(tutorial))))
 
 (define (example-names)
